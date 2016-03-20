f9243467f77d6f2eb6964558f335a7725b0ea6ed
tev <tvaillard@gmail.com>
2016-03-20 01:51:38 +0100

    Modif migration join table 3

--- db/migrate/20160308014344_create_join_table_appreciation_kewword.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableAppreciationKewword < ActiveRecord::Migration
-  def change
-    create_join_table :Appreciations, :Keywords do |t|
-      # t.index [:appreciation_id, :keyword_id]
-      # t.index [:keyword_id, :appreciation_id]
-    end
-  end
-end
--- db/migrate/20160308014344_create_join_table_appreciation_keyword.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableAppreciationKeyword < ActiveRecord::Migration
-  def change
-    create_join_table :appreciations, :keywords do |t|
-      # t.index [:appreciation_id, :keyword_id]
-      # t.index [:keyword_id, :appreciation_id]
-    end
-  end
-end
